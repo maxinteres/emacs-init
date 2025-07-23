a1f63665ddded95fead5ce003dcfb592e963cc23
maxinteres <zhumax@me.com>
2025-07-23 17:12:28 +0800

    keybinding 7/23

--- LISP/init-keybindings.el
+++ LISP/init-keybindings.el
@@ -2,11 +2,12 @@
 (global-set-key (kbd "<f2>") 'open-init-file)
 (global-set-key (kbd "C-h C-f") 'find-function)
 (global-set-key (kbd "C-h C-v") 'find-variable)
-(global-set-key (kbd "C-c z") 'undo)
+(global-set-key (kbd "C-c C-z") 'undo)
 (global-set-key (kbd "C-`") 'switch-to-scratch)
 (global-set-key (kbd "C-;") 'embark-act)
 (global-set-key (kbd "C-c C-r") 'replace-string)
 (global-set-key (kbd "C-c p f") 'project-find-file)
+(global-set-key (kbd "C-c z") 'shell)
 (with-eval-after-load 'hideshow
   (define-key hs-minor-mode-map (kbd "C-<tab>") 'fold-switch))
 
